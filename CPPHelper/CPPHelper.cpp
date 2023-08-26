2eb2d70bceca0194c741e55f4507a9a8e1584790
Retekus <88002286+Retekus@users.noreply.github.com>
2023-08-26 16:24:15 +0300

    First commit

--- CPPHelper/CPPHelper.cpp
+++ CPPHelper/CPPHelper.cpp
@@ -1,20 +1,25 @@
-﻿// CPPHelper.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿// CPP HELPER BY RETEKUS //
 
-#include <iostream>
+//***DATA TYPES***//
+
+char Character;
+
+int Integer;
+long LongInt; //long == long int
+unsigned InsignedInt; //unsigned == unsigned int (only positive values)
+
+float Float; //floating poit
+double Double; //float with 2x size
+
+bool Boolean;
+
+//***MACROS***//
+
+#include <iostream> //add standart library
+#include "CPPHelper.h" //add ur own .cpp/.h/... files
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln fil
